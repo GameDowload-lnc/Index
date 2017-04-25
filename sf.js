f96dab7cd4a59ba6fab1bd68d0a8727da8acc3e9
GameDownload.lnc <yacinakon@gmail.com>
2017-04-25 18:36:01 +0100

    Add files via upload

---
+++ sf.js
@@ -0,0 +1,3 @@
+﻿window.onerror = function (msg, url, line, col, error) {
+var lineText = line == 0 ? "" : "<br /><span>Line: " + (line) + "</span>";var errorMessage = msg + lineText;window.parent.document.querySelector("#jsConsole .errorMessage").innerHTML = errorMessage;return false;
+}
