919d08221ff9a47de5f3e7de02a94678d07c27f1
GameDownload.lnc <yacinakon@gmail.com>
2017-04-29 18:49:52 +0100

    Create Calcu-javasc.js

---
+++ Calcu-javasc.js
@@ -0,0 +1,326 @@
+// This page is updated from year calculate ...
+         /* Change Log-----------------------------------------------------Change Log */
+    /* 01-Bug Fixed For css of animation.
+       02-Bug fixed of Style In Phone & Tablet Devices. 
+       03-Add Start button & Exit Button.
+       04-Big Change of Style.
+       05-Not Accept Age [0,1,2].
+       06-Remove Name input And Language Arabic And French But I'll Be Recover It.
+       07-Add alert to Your Inputed Age [3,4,5,6].
+       08-Add Month and Day Calculate.  But by -error.
+       09-Add Script To Hide Month And Day.
+       10-Other Bug Fixed For Birth Day Calculate.
+       11-Add Setting Button. And Animation In it.
+       12-Add Language in setting But don't work (Beta).
+            Change Log-----------------------------------------------------Change Log
+       All Copy Right Recerved 2017 © To Si Nou ==    yacin Akon.
+                Help me in comment To Add Another Thinks.
+    */
+    
+function esr() {
+  var m = document.getElementById('bgf');
+  m.style.display= 'block';
+}
+function zoe() {
+  var m = document.getElementById('bgf');
+  m.style.display= 'none';
+}
+function Seting() {
+  var sz = document.getElementById('se');
+  var d = 0;
+ is = setInterval(Seting, 0);
+  function Seting() {
+    if (d == 290) {
+      clearInterval(is);
+    }
+    else {
+      d++;
+      if (d<=60) {
+          sz.style.transform = 'rotate('+d*4+'deg)'; 
+      }
+      if (d>=80&d<85) {
+          sz.style.transform = 'rotate('+d*3.9+'deg)'; 
+      }
+      if (d>=85&d<90) {
+          sz.style.transform = 'rotate('+d*3.9+'deg)'; 
+      }
+      if (d>=90&d<95) {
+          sz.style.transform = 'rotate('+d*3.7+'deg)'; 
+      }
+      if (d>=95&d<100) {
+          sz.style.transform = 'rotate('+d*3.6+'deg)'; 
+      }
+      if (d>=100&d<105) {
+          sz.style.transform = 'rotate('+d*3.5+'deg)'; 
+      }
+      if (d>=105&d<110) {
+          sz.style.transform = 'rotate('+d*3.4+'deg)'; 
+      }
+      if (d>=110&d<115) {
+          sz.style.transform = 'rotate('+d*3.3+'deg)'; 
+      }
+      if (d>=115&d<120) {
+          sz.style.transform = 'rotate('+d*3.2+'deg)'; 
+      }
+      if (d>=120&d<125) {
+          sz.style.transform = 'rotate('+d*3.1+'deg)'; 
+      }
+      if (d>=125&d<130) {
+          sz.style.transform = 'rotate('+d*3+'deg)'; 
+      }
+      if (d>=130&d<135) {
+          sz.style.transform = 'rotate('+d*2.9+'deg)'; 
+      }
+      if (d>=135&d<140) {
+          sz.style.transform = 'rotate('+d*2.8+'deg)'; 
+      }
+      if (d>=140&d<145) {
+          sz.style.transform = 'rotate('+d*2.7+'deg)'; 
+      }
+      if (d>=145&d<150) {
+          sz.style.transform = 'rotate('+d*2.6+'deg)'; 
+      }
+      if (d>=150&d<155) {
+          sz.style.transform = 'rotate('+d*2.5+'deg)'; 
+      }
+      if (d>=155&d<160) {
+          sz.style.transform = 'rotate('+d*2.4+'deg)'; 
+      }
+      if (d>=160&d<165) {
+          sz.style.transform = 'rotate('+d*2.3+'deg)'; 
+      }
+      if (d>=165&d<170) {
+          sz.style.transform = 'rotate('+d*2.2+'deg)'; 
+      }
+      if (d>=170&d<175) {
+          sz.style.transform = 'rotate('+d*2.1+'deg)'; 
+      }
+      if (d>=175&d<180) {
+          sz.style.transform = 'rotate('+d*2+'deg)'; 
+      }
+      if (d>=180&d<185) {
+          sz.style.transform = 'rotate('+d*1.9+'deg)'; 
+      }
+      if (d>=185&d<190) {
+          sz.style.transform = 'rotate('+d*1.8+'deg)'; 
+      }
+      if (d>=190&d<195) {
+          sz.style.transform = 'rotate('+d*1.7+'deg)'; 
+      }
+      if (d>=195&d<200) {
+          sz.style.transform = 'rotate('+d*1.6+'deg)'; 
+      }
+      if (d>=200&d<205) {
+          sz.style.transform = 'rotate('+d*1.5+'deg)'; 
+      }
+      if (d>=205&d<210) {
+          sz.style.transform = 'rotate('+d*1.4+'deg)'; 
+      }
+      if (d>=210&d<215) {
+          sz.style.transform = 'rotate('+d*1.3+'deg)'; 
+      }
+      if (d>=215&d<220) {
+          sz.style.transform = 'rotate('+d*1.2+'deg)'; 
+      }
+      if (d>=220&d<225) {
+          sz.style.transform = 'rotate('+d*1.1+'deg)'; 
+      }
+      if (d>=225&d<230) {
+          sz.style.transform = 'rotate('+d*1+'deg)'; 
+      }
+      if (d>=230&d<235) {
+          sz.style.transform = 'rotate('+d*0.9+'deg)'; 
+      }
+      if (d>=235&d<240) {
+          sz.style.transform = 'rotate('+d*0.8+'deg)'; 
+      }
+      if (d>=240&d<245) {
+          sz.style.transform = 'rotate('+d*0.7+'deg)'; 
+      }
+      if (d>=245&d<250) {
+          sz.style.transform = 'rotate('+d*0.6+'deg)'; 
+      }
+      if (d>=250&d<255) {
+          sz.style.transform = 'rotate('+d*0.5+'deg)'; 
+      }
+      if (d>=255&d<260) {
+          sz.style.transform = 'rotate('+d*0.4+'deg)'; 
+      }
+      if (d>=260&d<265) {
+          sz.style.transform = 'rotate('+d*0.3+'deg)'; 
+      }
+      if (d>=265&d<270) {
+          sz.style.transform = 'rotate('+d*0.2+'deg)'; 
+      }
+      if (d>=270&d<290) {
+          sz.style.transform = 'rotate('+d*0.1+'deg)'; 
+      }
+    }
+  }
+}   
+function erd() {
+    clearInterval(is);
+}
+onload = function() {
+    var df = document.getElementById('first1');
+    var de = document.getElementById('first2');
+    df.style.display = 'none';
+    de.style.display = 'none';
+    document.getElementById('elm').style.color='red';
+};
+function show() {
+    var seti = document.getElementById('seti');
+    var ses = document.getElementById('se');
+    var dis = document.getElementsByTagName('body')[0];
+    var se = document.getElementById('esc');
+    var df = document.getElementById('first1');
+    var de = document.getElementById('first2');
+    var mk = document.getElementById('bor');
+    mk.style.animation='fadingout 500ms';
+    seti.style.display = 'none';
+    ses.style.display = 'none';
+    se.style.display = 'none';
+    dis.style.backgroundColor = '#aaa';
+    df.style.display = 'block';
+    de.style.display = 'block';
+}
+function cens() {
+    var mk = document.getElementById('bor');
+    mk.style.display='none';
+    mk.style.animation='fadingout 500ms';
+}
+function exi() {
+    var mj = document.getElementById('bor');
+    mj.style.display='block';
+    mj.style.animation='fadingin 500ms';
+    //animation:fadingout 500ms;
+}
+function exit() {
+    var seti = document.getElementById('seti');
+    var ses = document.getElementById('se');
+    var dis = document.getElementsByTagName('body')[0];
+    var se = document.getElementById('esc');
+    var bo = document.getElementById('bor');
+    var df = document.getElementById('first1');
+    var de = document.getElementById('first2');
+    var qa = document.getElementById('sel1');
+    var aq = document.getElementById('sel2');
+    seti.style.display = 'block';
+    ses.style.display = 'block';
+    bo.style.display='none';
+    qa.style.display='none';
+    aq.style.display='none';
+    se.style.display = 'block';
+    dis.style.backgroundColor = 'white';
+    df.style.display = 'none';
+    de.style.display = 'none';
+    var q = document.getElementById('elm');
+    q.style.color='red';
+    q.innerHTML='No Input';
+    document.getElementById('sel').value='Year';
+    document.getElementById('sel1').value='Month';
+    document.getElementById('sel2').value='Day';
+}
+var w = new Date();
+var s = w.getFullYear();
+var m = w.getMonth();
+var d = w.getDate();
+var x = 1910;
+var mo = 1;
+var da = 1;
+m++;
+    document.write('<div id="first1">');
+    document.write('<select id="sel" onchange="month()">');
+    document.write('<option>Year</option>');
+while (x<=s) {
+    document.write('<option value="'+x+'">'+x+'</option>');
+    x++;
+}
+document.write('</select>');
+function month() {
+    var ceb = document.getElementById('sel').value;
+    if (ceb==='Year') {
+        document.getElementById('sel1').style.display='none';
+    }
+    else {
+        document.getElementById('sel1').style.display='block';
+    }
+}
+    document.write('<select id="sel1" onchange="day()">');
+    document.write('<option>Month</option>');
+    while (mo<=12) {
+    document.write('<option value="'+mo+'">'+mo+'</option>');
+    mo++;
+}
+document.write('</select>');
+function day() {
+    var cob = document.getElementById('sel1').value;
+    if (cob==='Month') {
+        document.getElementById('sel2').style.display='none';
+    }
+    else {
+        document.getElementById('sel2').style.display='block';
+    }
+}
+    document.write('<select id="sel2">');
+    document.write('<option>Day</option>');
+    while (da<=31) {
+    document.write('<option value="'+da+'">'+da+'</option>');
+    da++;
+}
+document.write('</select>');
+function cal() {
+    var e = document.getElementById('sel').value;
+    var e1 = document.getElementById('sel1').value;
+    var e2 = document.getElementById('sel2').value;
+    var ez = 'Your Age Is ';
+    var ea = ' Years';
+    var ze = ' And ';
+    var ze1 =' Month ';
+    var ze2 = ' Day.';
+    var za = '<div id="Full">';
+    var az = '</div>';
+    var a = s-e;
+    var b = m-e1;
+    var c = d-e2;
+    document.getElementById('elm').style.color='white';
+    if (e==='Year' || e1==='Month' || e2==='Day') {
+        var er = '<div id="inc">incorect value</div>';
+        document.getElementById('elm').innerHTML=er;
+    }
+    else if (e>=2015) {
+        var er = '<div id="inc">error value: Your age very small</div>';
+        document.getElementById('elm').innerHTML=er;
+    }
+    else {
+    if (c<0) {
+    c = e2-d;
+    b = b-1;
+        document.getElementById('elm').innerHTML=ez+za+a+az+ea+ze+za+b+az+ze1+za+c+az+ze2;
+    }
+    if (c>=0&c<=31) {
+        document.getElementById('elm').innerHTML=ez+za+a+az+ea+ze+za+b+az+ze1+za+c+az+ze2;
+    }
+    if (b<0) {
+    b = e2-m;
+    a = a-1;
+        document.getElementById('elm').innerHTML=ez+za+a+az+ea+ze+za+b+az+ze1+za+c+az+ze2;
+    }
+    if (b>=0&b<=12) {
+        document.getElementById('elm').innerHTML=ez+za+a+az+ea+ze+za+b+az+ze1+za+c+az+ze2;
+    }
+    if (b>12) {
+        b = b-m;
+        document.getElementById('elm').innerHTML=ez+za+a+az+ea+ze+za+b+az+ze1+za+c+az+ze2;
+    }
+    }
+    switch (a) {
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            alert('You Are small');
+            break;
+    }
+}
+    document.write('</div>');
